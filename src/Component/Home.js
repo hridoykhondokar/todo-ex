555ce436d7600020541987a130acdf827bce0197
Shahinur Alam Bhuiyan <shahinuralambhuiyan@Shahinurs-MacBook-Pro.local>
2022-09-16 12:58:03 +0600

    add h2

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -13,7 +13,8 @@ const Home = () => {
         <div>
             <div className="container ">
                 <section className="mt-3 text-center">
-                    <h2 style={{ textAlign: "center" }}>Enter Your Task</h2>
+                    <h2 style={{ textAlign: "center" }}>Enter Your Task for todo</h2>
+                    
                     <div className="d-flex todo col-lg-5 mx-auto justify-content-between align-items-center">
                         <input value={data} onChange={(e) => setData(e.target.value)} className="form-control" name="task" />
                         <Button className="mx-2" variant="contained" style={{ backgroundColor: "#ee5253" }}><AddIcon></AddIcon></Button>
